b33a18de58d4cdf31157d43e71f5dabdbcde165a
Jesus Antonio Soria Garcini <chuchocool@hotmail.com>
2019-12-26 16:29:46 -0600

    Remove some code

--- script.py
+++ script.py
@@ -1,22 +1,4 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-name = input("Your Name? ")
-print("Hello, ", name)
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-print(greet("World"))
-print(greet("Corey"))
 
 r = requests.get("https://coreyms.com")
 
